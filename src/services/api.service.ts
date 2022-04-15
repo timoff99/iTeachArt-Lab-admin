7c0d464bacbe8f07e035397220f7c5e5109e4abc
Pavel <pavel@wse-161-71b.icx.local>
2022-04-15 11:21:24 +0300

    fix: add Autorization token

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 import { ROUTE_NAMES } from "../router/routeNames";
 import { ApiUrl } from "../shared/types/routes";
@@ -8,6 +9,12 @@ export const API_URL = `${process.env.REACT_APP_API_URI}/api` || ApiUrl.dev;
 const api = axios.create({
   withCredentials: true,
   baseURL: API_URL,
+});
+
+api.interceptors.request.use((config) => {
+  // @ts-ignore: Unreachable code error
+  config.headers.Authorization = `Bearer ${Cookies.get("token")}`;
+  return config;
 });
 
 api.interceptors.response.use(
